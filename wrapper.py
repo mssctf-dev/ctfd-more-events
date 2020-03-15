16e62d01706f7ecb3d6b2cf62ac4e1ea79d6f0bf
Frank <frankli0324@hotmail.com>
2020-03-15 14:58:23 +0800

    fix wrapper

--- wrapper.py
+++ wrapper.py
@@ -1,11 +1,13 @@
+import logging
+
+
 def wrap(func, lm):
     def new_func(*args, **kwargs):
         ret = func(*args, **kwargs)
-        lm(ret)
         try:
-            pass
+            lm(ret)
         except Exception as e:
-            print(e)
+            logging.exception(e)
             pass
         return ret
     return new_func
